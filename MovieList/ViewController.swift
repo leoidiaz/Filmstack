c0d0b1134c5235c7fbd4e6dd75483ad478237c3d
leoidiaz <zaid.leonardo@gmail.com>
2021-01-21 14:59:57 -0800

    Initial Commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Leonardo Diaz on 1/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
