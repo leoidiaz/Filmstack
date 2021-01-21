11731a84b327f832560947efa52cca6cf5743814
leoidiaz <zaid.leonardo@gmail.com>
2021-01-21 15:27:38 -0800

    added HomeViewController

---
+++ MovieList/Views/HomeViewController/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  MovieList
+//
+//  Created by Leonardo Diaz on 1/21/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
