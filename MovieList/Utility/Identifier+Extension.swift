4a5774f6114109fe8ced71218b7e102bc732b175
leoidiaz <zaid.leonardo@gmail.com>
2021-01-22 15:07:46 -0800

    added IdentifierExtension file and MovieTableViewCell file / rename background image

---
+++ MovieList/Utility/Identifier+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Identifier+Extension.swift
+//  MovieList
+//
+//  Created by Leonardo Diaz on 1/22/21.
+//
+
+import Foundation
---
+++ MovieList/Views/HomeViewController/Cells/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  MovieList
+//
+//  Created by Leonardo Diaz on 1/22/21.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
