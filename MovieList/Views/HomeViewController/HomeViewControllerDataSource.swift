0d3a33bc993d7845da6140a76210a365b26e515e
leoidiaz <zaid.leonardo@gmail.com>
2021-01-21 16:18:50 -0800

    HomeViewControllerDataSource config

--- MovieList/Views/HomeViewController/HomeViewControllerDataSource.swift
+++ MovieList/Views/HomeViewController/HomeViewControllerDataSource.swift
@@ -5,4 +5,28 @@
 //  Created by Leonardo Diaz on 1/21/21.
 //
 
-import Foundation
+import UIKit
+
+enum Section {
+    case main
+}
+
+class HomeViewControllerDataSource:UICollectionViewDiffableDataSource<Section, Movie> {
+    init(collectionView: UICollectionView) {
+        super.init(collectionView: collectionView) { (collectionView, indexPath, movie) -> UICollectionViewCell? in
+            let cellConfiguration = UICollectionView.CellRegistration<UICollectionViewListCell, Movie> { (cell, indexPath, movie) in
+                var content = cell.defaultContentConfiguration()
+                content.text = movie.title
+                cell.contentConfiguration = content
+            }
+            let cell = collectionView.dequeueConfiguredReusableCell(using: cellConfiguration, for: indexPath, item: movie)
+            return cell
+        }
+    }
+    
+    func applyData(animated: Bool = true) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Movie>()
+        snapshot.appendSections([.main])
+        apply(snapshot, animatingDifferences: animated)
+    }
+}
