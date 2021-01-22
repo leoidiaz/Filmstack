e9bbdd6aebb53aab1ed077cf4c07ae3885d953f3
leoidiaz <zaid.leonardo@gmail.com>
2021-01-21 16:17:27 -0800

    Movie model config

--- MovieList/Models/Movie.swift
+++ MovieList/Models/Movie.swift
@@ -5,4 +5,12 @@
 //  Created by Leonardo Diaz on 1/21/21.
 //
 
-import Foundation
+import UIKit
+
+struct Movie: Hashable {
+    let title: String
+    let director: String
+    let releaseDate: String
+    let backgroundImage: UIImage
+    let posterImage: UIImage
+}
