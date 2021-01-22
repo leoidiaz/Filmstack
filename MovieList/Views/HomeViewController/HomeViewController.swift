b6e545cec09984c1313a45691780b8ab3f6ad673
leoidiaz <zaid.leonardo@gmail.com>
2021-01-21 16:19:30 -0800

    HomeViewController collectionViewList config

--- MovieList/Views/HomeViewController/HomeViewController.swift
+++ MovieList/Views/HomeViewController/HomeViewController.swift
@@ -7,12 +7,39 @@
 
 import UIKit
 
-class HomeViewController: UICollectionViewController {
-
+class HomeViewController: UIViewController {
+    //MARK: - Properties
+    
+    private var collectionView: UICollectionView!
+    
+    private lazy var dataSource: HomeViewControllerDataSource = {
+        return HomeViewControllerDataSource(collectionView: collectionView)
+    }()
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "MovieList"
-        view.backgroundColor = .systemBackground
+        setupView()
+        layoutView()
+    }
+    
+    //MARK: - Helper Methods
+    
+    private func setupView(){
+        let listLayout = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewCompositionalLayout.list(using: listLayout))
+        collectionView.dataSource = dataSource
+        collectionView.delegate = self
+        collectionView.backgroundColor = .systemGroupedBackground
+        view.addSubview(collectionView)
+        dataSource.applyData(animated: false)
+    }
+    
+    
+    private func layoutView(){
     }
 
 }
+
+extension HomeViewController: UICollectionViewDelegate { }
