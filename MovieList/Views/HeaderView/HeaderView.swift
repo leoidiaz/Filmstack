9a108ba1997e2834442cdc10137b164ba45ca464
leoidiaz <zaid.leonardo@gmail.com>
2021-01-25 17:01:35 -0800

    Add headerView

---
+++ MovieList/Views/HeaderView/HeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  HeaderView.swift
+//  MovieList
+//
+//  Created by Leonardo Diaz on 1/23/21.
+//
+
+import UIKit
+
+class HeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
