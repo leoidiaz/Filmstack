e849eacad594ec056a19fc1f30a6b13bd67f886a
leoidiaz <zaid.leonardo@gmail.com>
2021-01-27 12:25:16 -0800

    MovieViewModel

--- MovieList/ViewModels/MovieViewModel.swift
+++ MovieList/ViewModels/MovieViewModel.swift
@@ -5,4 +5,27 @@
 //  Created by Leonardo Diaz on 1/22/21.
 //
 
-import Foundation
+import UIKit
+
+struct MovieViewModel: Hashable {
+    
+    //MARK: - Properties
+    var movie: Movie
+    
+    var title: String {
+        return movie.title
+    }
+    
+    var director: String {
+        return movie.director
+    }
+    
+    //TODO: Release Data from Date -> String
+    var release: String {
+        return movie.releaseDate
+    }
+    
+    var background: UIImage {
+        return movie.backgroundImage
+    }
+}
