d3e2e11ffd3efd3d2609c9b3f3555fd2e63890d3
leoidiaz <zaid.leonardo@gmail.com>
2021-01-27 12:13:49 -0800

    movie cell config

--- MovieList/Views/HomeViewController/Cells/MovieTableViewCell.swift
+++ MovieList/Views/HomeViewController/Cells/MovieTableViewCell.swift
@@ -6,18 +6,53 @@
 //
 
 import UIKit
+import SnapKit
 
 class MovieTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    //MARK: - Properties
+    lazy var background = UIImageView()
+    lazy var title = UILabel()
+    
+    var movieViewModel: MovieViewModel? {
+        didSet{
+            configureCell()
+        }
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubview(background)
+        addSubview(title)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    //MARK: - Helper Methods
+    func configureCell() {
+        guard let movieView = movieViewModel  else { return }
+        background.contentMode = .scaleToFill
+        background.image = movieView.background
+        title.text = movieView.title
+        title.font = UIFont.preferredFont(forTextStyle: .title1)
+        title.textColor = .white
     }
 
+    
+    func layout(){
+        background.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().inset(5)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview().inset(5)
+        }
+        
+        title.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview().offset(15)
+            make.centerY.equalToSuperview().offset(50)
+        }
+    }
 }
