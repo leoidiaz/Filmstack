c8f33a5537a48ffe61d5c263dfaedc72a0b297e4
leoidiaz <zaid.leonardo@gmail.com>
2021-01-27 12:14:49 -0800

    remove navigationController

--- MovieList/Supporting/SceneDelegate.swift
+++ MovieList/Supporting/SceneDelegate.swift
@@ -15,9 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let rootViewController = HomeViewController()
-        window?.rootViewController = UINavigationController(rootViewController: rootViewController)
-        rootViewController.navigationController?.navigationBar.prefersLargeTitles = true
+        window?.rootViewController = HomeViewController()
         window?.makeKeyAndVisible()
     }
 
