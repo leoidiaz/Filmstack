79ff56fc4eaf3e957967195e782d9acb483f9b29
leoidiaz <zaid.leonardo@gmail.com>
2021-01-28 16:19:33 -0800

    AddMovieView config

--- MovieList/Views/AddMovieView/AddMovieView.swift
+++ MovieList/Views/AddMovieView/AddMovieView.swift
@@ -6,15 +6,92 @@
 //
 
 import UIKit
+import SnapKit
 
 class AddMovieView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    //MARK: - Properties
+    lazy var container = UIView()
+    lazy var closeButton = UIButton(type: .close)
+    lazy var titleLabel = UILabel()
+    lazy var addButton = UIButton()
+    
+    //MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.frame = UIScreen.main.bounds
+        setupView()
+        setupLayout()
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissView)))
+        animateView()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Helper Methods
+    
+    @objc func dismissView(){
+        UIView.animate(withDuration: 0.25, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseOut) { [weak self] in
+            guard let self = self else { return }
+            self.container.transform = CGAffineTransform(translationX: 0, y: self.frame.height)
+        } completion: { [weak self] (_) in
+            self?.subviews.forEach{$0.removeFromSuperview()}
+            self?.removeFromSuperview()
+        }
+    }
+    
+    @objc func animateView(){
+        container.transform = CGAffineTransform(translationX: 0, y: frame.height)
+        alpha = 0.8
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseInOut) { [weak self] in
+            self?.container.transform = .identity
+            self?.alpha = 1
+        }
+    }
+}
+//MARK: - Setup Methods
+extension AddMovieView {
+    private func setupView(){
+        container.layer.cornerRadius = 20
+        container.backgroundColor = .secondarySystemBackground
+        closeButton.addTarget(self, action: #selector(dismissView), for: .touchDown)
+        titleLabel.text = "Want to add some films?"
+        titleLabel.font = UIFont.customFont(textStyle: .title2)
+        addButton.setTitle("ADD", for: .normal)
+        addButton.setTitleColor(.systemBackground, for: .normal)
+        addButton.titleLabel?.font = UIFont.customFont(textStyle: .body)
+        addButton.backgroundColor = .label
+        addButton.layer.cornerRadius = 10
+        addSubview(container)
+        container.addSubview(closeButton)
+        container.addSubview(titleLabel)
+        container.addSubview(addButton)
+    }
+    
+    private func setupLayout(){
+        container.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.25)
+            make.width.equalToSuperview().multipliedBy(1)
+        }
+        
+        closeButton.snp.makeConstraints { (make) in
+            make.top.equalTo(container.snp.topMargin)
+            make.trailing.equalTo(container.snp.trailingMargin)
+        }
+        
+        titleLabel.snp.makeConstraints { (make) in
+            make.centerX.equalTo(container)
+            make.top.equalTo(container.snp.top).offset(60)
+        }
+        
+        addButton.snp.makeConstraints { (make) in
+            make.width.equalTo(container).multipliedBy(0.3)
+            make.centerX.equalTo(container)
+            make.bottom.equalTo(container.safeAreaLayoutGuide.snp.bottomMargin).offset(-15)
+        }
+    }
 }
