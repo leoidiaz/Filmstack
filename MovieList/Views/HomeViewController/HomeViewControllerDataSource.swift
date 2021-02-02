490d56272f7b455f70ab4f9752cfe593fde64a57
leoidiaz <zaid.leonardo@gmail.com>
2021-02-02 14:28:43 -0800

    Change from tableview datasource to collectionview

--- MovieList/Views/HomeViewController/HomeViewControllerDataSource.swift
+++ MovieList/Views/HomeViewController/HomeViewControllerDataSource.swift
@@ -11,16 +11,18 @@ enum Section {
     case main
 }
 
-class HomeViewControllerDataSource:UITableViewDiffableDataSource<Section, MovieViewModel> {
+class HomeViewControllerDataSource:UICollectionViewDiffableDataSource<Section, MovieViewModel> {
+    
     var movieManager: MovieManager
-    init(tableView: UITableView, movieManager: MovieManager = MovieManager()) {
+    
+    init(collectionView: UICollectionView, movieManager: MovieManager = MovieManager()) {
         self.movieManager = movieManager
-        super.init(tableView: tableView) { (tableView, indexPath, movieVM) -> UITableViewCell? in
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: MovieTableViewCell.reuseIdentifier) as? MovieTableViewCell else { return UITableViewCell() }
-            cell.movieViewModel = movieVM
-            cell.selectionStyle = .none
+        super.init(collectionView: collectionView) { (collectionView, indexPath, item) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieCollectionViewCell.reuseIdentifier, for: indexPath) as? MovieCollectionViewCell
+            cell?.movieViewModel = item
             return cell
         }
+        applyData()
     }
     
     func applyData(animated: Bool = true) {
