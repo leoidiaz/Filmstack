3353a66c4719038cd8da5685fd28b0d9b12b1875
leoidiaz <zaid.leonardo@gmail.com>
2021-02-02 14:23:53 -0800

    create movies method updated to include poster

--- MovieList/Managers/MovieManager.swift
+++ MovieList/Managers/MovieManager.swift
@@ -8,21 +8,15 @@
 import UIKit
 
 class MovieManager {
-    //    var allMovies: [Movie] = {[
-    //        Movie(title: "In the mood for love", director: "Wong Kar-wai", releaseDate: "2000", backgroundImage: UIImage(named: "background1")!),
-    //        Movie(title: "Jaws", director: "Steven Spielberg", releaseDate: "1975", backgroundImage: UIImage(named: "background2")!),
-    //        Movie(title: "Y tu mamá también", director: "Alfonso Cuarón", releaseDate: "2002", backgroundImage: UIImage(named: "background3")!),
-    //        Movie(title: "Psycho", director: "Alfred Hitchcock", releaseDate: "1960", backgroundImage: UIImage(named: "background4")!),
-    //    ]}()
     var movies = [MovieViewModel]()
     var createMovies: [MovieViewModel] = {
         [
-            MovieViewModel(movie: Movie(title: "In The Mood For Love", director: "Wong Kar-wai", releaseDate: "2000", backgroundImage: UIImage(named: "background1")!)),
-            MovieViewModel(movie: Movie(title: "Jaws", director: "Steven Spielberg", releaseDate: "1975", backgroundImage: UIImage(named: "background2")!)),
-            MovieViewModel(movie: Movie(title: "Y Tu Mamá También", director: "Alfonso Cuarón", releaseDate: "2002", backgroundImage: UIImage(named: "background3")!)),
-            MovieViewModel(movie:  Movie(title: "Psycho", director: "Alfred Hitchcock", releaseDate: "1960", backgroundImage: UIImage(named: "background4")!)),
-            MovieViewModel(movie:  Movie(title: "Memories of Murder", director: "Bong Joon-ho", releaseDate: "2003", backgroundImage: UIImage(named: "background5")!)),
-            MovieViewModel(movie:  Movie(title: "Drive", director: "Nicolas Winding Refn", releaseDate: "2011", backgroundImage: UIImage(named: "background6")!))
+            MovieViewModel(movie: Movie(title: "In The Mood For Love", director: "Wong Kar-wai", releaseDate: "2000", backgroundImage: UIImage(named: "background1")!, posterImage: UIImage(named: "poster1")!)),
+            MovieViewModel(movie: Movie(title: "Jaws", director: "Steven Spielberg", releaseDate: "1975", backgroundImage: UIImage(named: "background2")!, posterImage: UIImage(named: "poster2")!)),
+            MovieViewModel(movie: Movie(title: "Y Tu Mamá También", director: "Alfonso Cuarón", releaseDate: "2002", backgroundImage: UIImage(named: "background3")!, posterImage: UIImage(named: "poster3")!)),
+            MovieViewModel(movie:  Movie(title: "Psycho", director: "Alfred Hitchcock", releaseDate: "1960", backgroundImage: UIImage(named: "background4")!, posterImage: UIImage(named: "poster4")!)),
+            MovieViewModel(movie:  Movie(title: "Memories of Murder", director: "Bong Joon-ho", releaseDate: "2003", backgroundImage: UIImage(named: "background5")!, posterImage: UIImage(named: "poster5")!)),
+            MovieViewModel(movie:  Movie(title: "Drive", director: "Nicolas Winding Refn", releaseDate: "2011", backgroundImage: UIImage(named: "background6")!, posterImage: UIImage(named: "poster6")!))
         ]
     }()
 }
