1aba0eebd0a87c612d8d80ea416caca9bf873158
leoidiaz <zaid.leonardo@gmail.com>
2021-02-04 17:13:40 -0800

    Remove the bottom half views and refactor into another file

--- MovieList/Views/MovieDetailViewController/MovieDetailViewController.swift
+++ MovieList/Views/MovieDetailViewController/MovieDetailViewController.swift
@@ -13,13 +13,7 @@ class MovieDetailViewController: UIViewController {
     var movieViewModel: MovieViewModel
     lazy var closeButton = UIButton(type: .custom)
     lazy var backgroundImage = UIImageView()
-    lazy var posterImage = UIImageView()
-    lazy var titleLabel = UILabel()
-    lazy var directorLabel = UILabel()
-    lazy var releaseLabel = UILabel()
-    lazy var watchButton = UIButton()
-    lazy var labelStack = UIStackView()
-    lazy var imageStack = UIStackView()
+    lazy var movieDetailsView = MovieDetailsView()
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
@@ -49,6 +43,7 @@ extension MovieDetailViewController {
     
     private func setupView(){
         view.backgroundColor = .systemBackground
+
         closeButton.setImage(UIImage(systemName: "xmark.square.fill", withConfiguration: UIImage.SymbolConfiguration(scale: .large)), for: .normal)
         closeButton.addTarget(self, action: #selector(dismissView), for: .touchDown)
         closeButton.tintColor = .white
@@ -56,47 +51,13 @@ extension MovieDetailViewController {
         backgroundImage.contentMode = .scaleAspectFit
         
         view.addSubview(backgroundImage)
-        view.addSubview(titleLabel)
-        view.addSubview(labelStack)
-        view.addSubview(imageStack)
-        view.addSubview(posterImage)
         view.addSubview(closeButton)
-        view.addSubview(watchButton)
-        
-        titleLabel.text = movieViewModel.title
-        titleLabel.textAlignment = .center
-        titleLabel.font = UIFont.customFont(textStyle: .title1)
-        titleLabel.adjustsFontSizeToFitWidth = true
-        
-        directorLabel.text = movieViewModel.director
-        directorLabel.numberOfLines = 0
-        directorLabel.font = UIFont.customFont(textStyle: .title1)
-        
-        releaseLabel.font = UIFont.customFont(textStyle: .title2)
-        releaseLabel.text = movieViewModel.release
-        
-        watchButton.setTitle("Watch Now", for: .normal)
-        watchButton.backgroundColor = .label
-        watchButton.setTitleColor(.systemBackground, for: .normal)
-        
-        
-        labelStack.alignment = .leading
-        labelStack.axis = .vertical
-        labelStack.distribution = .fillEqually
-        labelStack.addArrangedSubview(directorLabel)
-        labelStack.addArrangedSubview(releaseLabel)
-        
-        posterImage.image = movieViewModel.posterImage
-        posterImage.clipsToBounds = true
-        posterImage.layer.cornerRadius = 2
-        posterImage.contentMode = .scaleToFill
-        
-        imageStack.alignment = .center
-        imageStack.axis = .horizontal
-        imageStack.distribution = .fill
-        imageStack.spacing = 15
-        imageStack.addArrangedSubview(labelStack)
-        imageStack.addArrangedSubview(posterImage)
+        view.addSubview(movieDetailsView)
+
+        movieDetailsView.titleLabel.text = movieViewModel.title
+        movieDetailsView.directorLabel.text = movieViewModel.director
+        movieDetailsView.releaseLabel.text = movieViewModel.release
+        movieDetailsView.posterImage.image = movieViewModel.posterImage
     }
     
     private func setupLayout(){
@@ -107,33 +68,15 @@ extension MovieDetailViewController {
             make.height.equalToSuperview().multipliedBy(0.3)
         }
         
-        posterImage.snp.makeConstraints { (make) in
-            make.width.equalTo(125)
-            make.height.equalTo(205)
-        }
-        
-        titleLabel.snp.makeConstraints { (make) in
-            make.width.equalToSuperview().multipliedBy(0.9)
-            make.centerX.equalToSuperview()
+        movieDetailsView.snp.makeConstraints { (make) in
             make.top.equalTo(backgroundImage.snp.bottomMargin).offset(30)
-        }
-        
-        
-        imageStack.snp.makeConstraints { (make) in
-            make.top.equalTo(titleLabel.snp.bottomMargin).offset(40)
-            make.centerX.equalToSuperview()
-            make.width.equalToSuperview().multipliedBy(0.9)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(view.snp.bottomMargin).offset(-50)
         }
         
         closeButton.snp.makeConstraints { (make) in
             make.trailing.equalTo(view.snp.trailingMargin)
             make.top.equalTo(view.snp.topMargin).offset(15)
-        }
-        
-        watchButton.snp.makeConstraints { (make) in
-            make.bottom.equalTo(view.snp.bottomMargin).offset(-50)
-            make.centerX.equalToSuperview()
-            make.width.equalToSuperview().multipliedBy(0.8)
         }
         
     }
