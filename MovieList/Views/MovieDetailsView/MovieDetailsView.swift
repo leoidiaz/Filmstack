243780a75fdcb0688cc25e18a8e279e321a67f36
leoidiaz <zaid.leonardo@gmail.com>
2021-02-04 17:12:22 -0800

    Create MovieDetailView file

---
+++ MovieList/Views/MovieDetailsView/MovieDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetailsView.swift
+//  MovieList
+//
+//  Created by Leonardo Diaz on 2/4/21.
+//
+
+import UIKit
+
+class MovieDetailsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
